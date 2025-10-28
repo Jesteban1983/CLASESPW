e18003e63f3fe2fc838761c7fb1f0f1a45c7b38e
Jesteban1983 <joonathanesteban@protonmail.com>
2025-10-28 10:55:23 +0100

    update

--- DAM/T1/Ejercicio del 1 - 6/src/Ejercicio.java
+++ DAM/T1/Ejercicio del 1 - 6/src/Ejercicio.java
@@ -1,6 +1,6 @@
 public class Ejercicio {
     public void ejecutarEjercicio1() {
-
+        System.out.println("Ejecucion del ejercicio 1");
         /*Ejercicio 1: Definir y mostrar variables: Crea un programa que
                        defina tres variables: nombre, edad y ciudad.
                        Asigna valores a cada una y muestra su contenido en la consola.*/
@@ -15,6 +15,31 @@ public class Ejercicio {
 
 
     public void ejecutarEjercicio2() {
-        System.out.println("Hola");
+        System.out.println("\nEjecucion del Ejercicio 2");
+        /*Crea un programa que defina una variable llamada puntuación con valor inicial 0.
+         Luego, modifica su valor tres veces y muestra el resultado final.
+         */
+        int puntuacion = 0 ;
+        System.out.println("Puntuacion Inicial: " +puntuacion);
+        puntuacion = puntuacion +10;
+        System.out.println("Despues de la primera modificacion: " +puntuacion);
+        puntuacion = puntuacion -3;
+        System.out.println("Despues de la segunda modificacion: " +puntuacion);
+        puntuacion = puntuacion *2;
+        System.out.println("Puntuacion final: " +puntuacion);
+    }
+
+    public void ejecutarEjercicio3() {
+        System.out.println("\nEjecucion del Ejercicio 3");
+        String nombre="Carlos";
+        int edad= 30;
+        boolean esEstuduante=true;
+        double altura= 1.75;
+        char letraInicial= 'C';
+        System.out.println("Nombre: " + nombre + " - tipo: "+ "String");
+        System.out.println("Edad: " + edad + " - tipo: "+ " Int");
+        System.out.println("¿Es estudiante?: " + esEstuduante + " - tipo: "+ "Boolean");
+        System.out.println("Altura: " + altura + " - tipo: "+ "Double");
+        System.out.println("Inicial: " + letraInicial + "- tipo: "+ "Char");
     }
 }
--- DAM/T1/Ejercicio del 1 - 6/src/Main.java
+++ DAM/T1/Ejercicio del 1 - 6/src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*Ejecucion del ejercicio 1");
         // 1. INSTANCIAR: Creamos UN SOLO objeto de la clase Ejercicios
         Ejercicio misEjercicios = new Ejercicio();
         misEjercicios.ejecutarEjercicio1();
-
+        misEjercicios.ejecutarEjercicio2();
+        misEjercicios.ejecutarEjercicio3();
     }
 }
