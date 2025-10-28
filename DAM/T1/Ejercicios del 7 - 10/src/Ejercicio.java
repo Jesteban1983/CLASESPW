fb13c9ba11093361e11992bfb0404fca8b786bb0
Jesteban1983 <joonathanesteban@protonmail.com>
2025-10-28 12:55:55 +0100

    ejercicio 7

--- DAM/T1/Ejercicios del 7 - 10/src/Ejercicio.java
+++ DAM/T1/Ejercicios del 7 - 10/src/Ejercicio.java
@@ -1,6 +1,20 @@
 public class Ejercicio {
     public void ejecutarEjercicio7(){
 
-        System.out.println("Estamos ejecutando el Ejercicio 7");
+        System.out.println("\nEjercicio 7");
+        /*Crea un programa que almacene tu información personal (nombre, apellido, edad y ciudad)
+         en variables y muestre un mensaje de presentación por consola.
+         */
+
+        String nombre = "Jonathan";
+        String apellido = "Esteban";
+        String ciudad = "Barcelona";
+        int edad = 42;
+
+        // Usamos la concatenación (+) para unir texto fijo con el valor de las variables.
+        System.out.println("\n!Hola¡ Me llamo " + nombre + " " + apellido + ".");
+        System.out.println("Tengo " + edad + " años y vivo en "+ ciudad + ".");
     }
 }
+
+
--- DAM/T1/Ejercicios del 7 - 10/src/Main.java
+++ DAM/T1/Ejercicios del 7 - 10/src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\n=======================================");
+        System.out.println("             Ejercicios                ");
+        System.out.println("=======================================");
             Ejercicio misEjercicios= new Ejercicio();
             misEjercicios.ejecutarEjercicio7();
         }
