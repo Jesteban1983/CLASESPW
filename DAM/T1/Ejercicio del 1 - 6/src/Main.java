86ccf7f34dcf8e65586f63b96d995e4cb72e466a
Jesteban1983 <joonathanesteban@protonmail.com>
2025-10-28 09:38:59 +0100

    update

---
+++ DAM/T1/Ejercicio del 1 - 6/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        
+        /*Ejercicio 1: Definir y mostrar variables: Crea un programa que
+                       defina tres variables: nombre, edad y ciudad.
+                       Asigna valores a cada una y muestra su contenido en la consola.*/
+
+                String nombre = "Jonathan";
+                int edad = 42;
+                String ciudad = "Barcelona";
+                System.out.println("Hola, mi nombre es " + nombre);
+                System.out.println("Tengo " + edad + " años");
+                System.out.println("Vivo en " + ciudad);
+
+    }
+}
