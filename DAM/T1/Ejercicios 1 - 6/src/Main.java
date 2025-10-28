8b193310a3f88bb0cc5a64dcb0b4f875bf2dffa6
Jesteban1983 <joonathanesteban@protonmail.com>
2025-10-28 09:36:13 +0100

    update

--- DAM/T1/Ejercicios 1 - 6/src/Main.java
+++ DAM/T1/Ejercicios 1 - 6/src/Main.java
@@ -1,15 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the g
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void Main(String[] args) {
+        /*Ejercicio 1: Definir y mostrar variables: Crea un programa que
+                       defina tres variables: nombre, edad y ciudad.
+                       Asigna valores a cada una y muestra su contenido en la consola.*/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String nombre = "Jonathan";
+        int edad = 42;
+        String ciudad = "Barcelona";
+        System.out.println("Hola, mi nombre es " + nombre);
+        System.out.println("Tengo " + edad + " años");
+        System.out.println("Vivo en " + ciudad);
     }
 }
