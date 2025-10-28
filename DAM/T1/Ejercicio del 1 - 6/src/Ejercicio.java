cca9910f67916ba0b4e6817dbab663ea3af5c8b0
Jesteban1983 <joonathanesteban@protonmail.com>
2025-10-28 10:15:17 +0100

    update

---
+++ DAM/T1/Ejercicio del 1 - 6/src/Ejercicio.java
@@ -0,0 +1,20 @@
+public class Ejercicio {
+    public void ejecutarEjercicio1() {
+
+        /*Ejercicio 1: Definir y mostrar variables: Crea un programa que
+                       defina tres variables: nombre, edad y ciudad.
+                       Asigna valores a cada una y muestra su contenido en la consola.*/
+
+        String nombre = "Jonathan";
+        int edad = 42;
+        String ciudad = "Barcelona";
+        System.out.println("Hola, mi nombre es " + nombre);
+        System.out.println("Tengo " + edad + " años");
+        System.out.println("Vivo en " + ciudad);
+    }
+
+
+    public void ejecutarEjercicio2() {
+        System.out.println("Hola");
+    }
+}
--- DAM/T1/Ejercicio del 1 - 6/src/Main.java
+++ DAM/T1/Ejercicio del 1 - 6/src/Main.java
@@ -2,17 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
-        /*Ejercicio 1: Definir y mostrar variables: Crea un programa que
-                       defina tres variables: nombre, edad y ciudad.
-                       Asigna valores a cada una y muestra su contenido en la consola.*/
-
-                String nombre = "Jonathan";
-                int edad = 42;
-                String ciudad = "Barcelona";
-                System.out.println("Hola, mi nombre es " + nombre);
-                System.out.println("Tengo " + edad + " años");
-                System.out.println("Vivo en " + ciudad);
+        System.out.println("*Ejecucion del ejercicio 1");
+        // 1. INSTANCIAR: Creamos UN SOLO objeto de la clase Ejercicios
+        Ejercicio misEjercicios = new Ejercicio();
+        misEjercicios.ejecutarEjercicio1();
 
     }
 }
