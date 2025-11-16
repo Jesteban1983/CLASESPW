f7f07d4053e960e2ef4bbb896d719d973b6c4788
Jesteban1983 <joonathanesteban@protonmail.com>
2025-11-16 17:41:32 +0100

    operadores

--- DAM/T2/Operadores/src/Main.java
+++ DAM/T2/Operadores/src/Main.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Iniciando programa de operadores");
 
            Operadores operadores = new Operadores();
-           operadores.operadoresAritmeticos();
-
+            Scanner lectorTeclado = new Scanner(System.in);
+            System.out.println("Dime tu nombre y apellido");
+            String nombre = lectorTeclado.nextLine();
+            System.out.println("Cuanto quieres ganar?");
+            double salarioEntrada = lectorTeclado.nextDouble();
+            System.out.println("Que edad tienes?");
+            int edad = lectorTeclado.nextInt();
+            System.out.println("Tienes carnet de conducir?");
+            boolean conducir = lectorTeclado.nextBoolean();
+            //operadores.operadoresAritmeticos();
+            //operadores.operadoresAsignacion();
+            //operadores.operadoresComparacion();
+            //operadores.operadoresLogicos();
+            operadores.evaluarCandidato(salarioEntrada,edad,conducir,nombre);
     }
 }
--- DAM/T2/Operadores/src/Operadores.java
+++ DAM/T2/Operadores/src/Operadores.java
@@ -22,11 +22,56 @@ public class Operadores {
         int resta=operandoUno-operandoDos;
         int multi=operandoUno*operandoDos;
         double div= (double)operandoUno / operandoDos;
-        int modulo= operandoUno%operandoDos;
+        int modulo= operandoUno%operandoDos; //me vale para saber un numero es par o impar
         System.out.println("El resultado de la suma es: " + suma);
         System.out.println("El resultado de la resta es: " + resta);
         System.out.println("El resultado de la multiplicacion es: " + multi);
         System.out.println("El resultado de la division es: " + div);
         System.out.println("El resultado del resto de la division es: " + modulo);
+
+    }
+    public void operadoresAsignacion(){
+        int operadorUno = 10;
+        int operadorDos= 6;
+        operadorUno += 5; // en operadorUno guarda la suma de lo que valia operadorUno  + 5
+        operadorUno += 15;
+        operadorUno += operadorDos;
+        System.out.println("El valor de operador 1 es "+operadorUno);
+        operadorUno-= 5; //16
+        operadorUno*=2; //32
+        operadorUno%=2; //0
+        System.out.println("El resultado de las asignaciones completas es de "+operadorUno);
+    }
+    public void operadoresComparacion(){
+        // seran usados como condiciones para ejecutar una parte del codigo u otra
+        int operandoUno = 10;
+        int operandoDos = 20;
+        boolean resultado = operandoUno>operandoDos;
+        System.out.println("El resultado de la comparacion > es "+resultado);
+        resultado = operandoUno<operandoDos;
+        System.out.println("El resultado de la comparacion < es "+resultado);
+        resultado = operandoUno<=operandoDos;
+        resultado = operandoUno>=operandoDos;
+        resultado = operandoUno==operandoDos;
+        resultado = operandoUno!=operandoDos;
+        // cuantos años tienes
+        // tienes carnet de conducir
+        // cuanto quieres cobrar
+    }
+    public void operadoresLogicos(){
+        int sueldo = 20000;
+        int edad = 35;
+        boolean conducir = true;
+        System.out.println("El candidato puede conducir: "+!conducir);
+        // cuando quiere cobrar menos de 30000 y tiene menos de 40 años y puede conducir
+        boolean cadidatoValido=sueldo<30000 && edad<40 && conducir;
+        System.out.println("El candidato es valido: " +cadidatoValido);// para la logica AND
+        cadidatoValido=sueldo<20000 || edad<25 || conducir;
+        System.out.println("El candidato es valido: " +cadidatoValido);// para la logica OR
+    }
+    public void evaluarCandidato(double sueldo, int edad, boolean conducir, String nombre){
+        boolean resultado = sueldo<=20000 && edad<40 && conducir;
+        System.out.println("Evaluando al candidato: "+nombre);
+        System.out.println("La evaluacion del candidato es: "+resultado);
     }
 }
